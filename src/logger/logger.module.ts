22a1e940b21597ccf43e52d494ba1c37b24de0f3
aleferraro7 <[alejandroferraro7@gmail.com]>
2024-05-09 11:43:24 +0200

    testing

--- src/logger/logger.module.ts
+++ src/logger/logger.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
-import { LoggerService } from './logger.service';
-import { PinoLoggerService } from './pino-logger.service';
-import { LoggerModule } from 'nestjs-pino';
+// import { Module } from '@nestjs/common';
+// import { LoggerService } from './logger.service';
+// // import { PinoLoggerService } from './pino-logger.service';
+// import { LoggerModule } from 'nestjs-pino';
 
-@Module({
-  providers: [LoggerService, PinoLoggerService],
-  exports: [LoggerService, PinoLoggerService],
-  imports: [LoggerModule.forRoot()],
-})
-export class PinoLoggerModule {}
+// @Module({
+//   providers: [LoggerService],
+//   exports: [LoggerService],
+//   imports: [LoggerModule.forRoot()],
+// })
+// export class PinoLoggerModule {}
--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,12 +1,12 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { Logger, PinoLogger, PARAMS_PROVIDER_TOKEN, Params } from 'nestjs-pino';
+// import { Inject, Injectable } from '@nestjs/common';
+// import { Logger, PinoLogger, PARAMS_PROVIDER_TOKEN, Params } from 'nestjs-pino';
 
-@Injectable()
-export class LoggerService extends Logger {
-  constructor(
-    logger: PinoLogger,
-    @Inject(PARAMS_PROVIDER_TOKEN) params: Params,
-  ) {
-    super(logger, params);
-  }
-}
+// @Injectable()
+// export class LoggerService extends Logger {
+//   constructor(
+//     logger: PinoLogger,
+//     @Inject(PARAMS_PROVIDER_TOKEN) params: Params,
+//   ) {
+//     super(logger, params);
+//   }
+// }
