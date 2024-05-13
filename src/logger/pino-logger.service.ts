35b4e691c12c4dd98b54ad369e13198524e7bd64
aleferraro7 <[alejandroferraro7@gmail.com]>
2024-05-13 11:58:16 +0200

    deleting logger module

--- src/logger/pino-logger.service.ts
+++
@@ -1,9 +0,0 @@
-// import { Inject, Injectable } from '@nestjs/common';
-// import { PARAMS_PROVIDER_TOKEN, Params, PinoLogger } from 'nestjs-pino';
-
-// @Injectable()
-// export class PinoLoggerService extends PinoLogger {
-//   constructor(@Inject(PARAMS_PROVIDER_TOKEN) params: Params) {
-//     super(params);
-//   }
-// }
